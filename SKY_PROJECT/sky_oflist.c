8e7eb3536a42861b8be43e53824b80b3f04c4021
moinuddin <moincse786@gmail.com>
2022-05-16 14:48:21 +0530

    added sky

---
+++ SKY_PROJECT/sky_oflist.c
@@ -0,0 +1,105 @@
+# number of field entries allocated 28, used 28
+0 PIN_FLD_POID           POID [0] 0.0.0.1 /plan 306283 0
+0 PIN_FLD_LOCALES       ARRAY [1] allocated 20, used 1
+1     PIN_FLD_LOCALE          STR [0] "en_US"
+0 PIN_FLD_CODE            STR [0] "3b2c1c5e-fed7-4ccc-a876-dcf402cf1f01"
+0 PIN_FLD_SERVICES      ARRAY [0] allocated 20, used 7
+1     PIN_FLD_DEAL_OBJ       POID [0] 0.0.0.0  0 0
+1     PIN_FLD_LOGIN           STR [0] "654-20220513-035410-0-4464--155572528-cgbu-brm75pdc.us.oracle.com"
+1     PIN_FLD_SUBSCRIPTION_INDEX    INT [0] 0
+1     PIN_FLD_BAL_INFO_INDEX    INT [0] 0
+1     PIN_FLD_BAL_INFO      ARRAY [0]     NULL array ptr
+1     PIN_FLD_SERVICE_OBJ    POID [0] 0.0.0.1 /service/telco/gsm/telephony 1019901 0
+1     PIN_FLD_BILLINFO_OBJ   POID [0] 0.0.0.1 /billinfo 1019773 0
+0 PIN_FLD_STATUSES      ARRAY [0] allocated 20, used 2
+1     PIN_FLD_STATUS         ENUM [0] 10102
+1     PIN_FLD_STATUS_FLAGS    INT [0] 1
+0 PIN_FLD_WRITE_ACCESS    STR [0] "S"
+0 PIN_FLD_READ_ACCESS     STR [0] "B"
+0 PIN_FLD_NAME            STR [0] "Wireless Basic Package"
+0 PIN_FLD_FLAGS           INT [0] 0
+0 PIN_FLD_NAMEINFO      ARRAY [1] allocated 20, used 10
+1     PIN_FLD_LAST_NAME       STR [0] "Md"
+1     PIN_FLD_MIDDLE_NAME     STR [0] "Khaja"
+1     PIN_FLD_FIRST_NAME      STR [0] "Moinuddin"
+1     PIN_FLD_EMAIL_ADDR      STR [0] "ed@oracle.com"
+1     PIN_FLD_COUNTRY         STR [0] "USA"
+1     PIN_FLD_ZIP             STR [0] "12345"
+1     PIN_FLD_STATE           STR [0] "NC"
+1     PIN_FLD_CITY            STR [0] "Raleigh"
+1     PIN_FLD_ADDRESS         STR [0] "123 Main"
+1     PIN_FLD_COMPANY         STR [0] "Oracle"
+0 PIN_FLD_MOD_T        TSTAMP [0] (1388722147) Thu Jan  2 20:09:07 2014
+0 PIN_FLD_BAL_INFO      ARRAY [0] allocated 20, used 9
+1     PIN_FLD_NAME            STR [0] "Balance Group<Account>"
+1     PIN_FLD_POID           POID [0] 0.0.0.1 /balance_group 1016573 0
+1     PIN_FLD_LIMIT         ARRAY [840] allocated 20, used 4
+2         PIN_FLD_CREDIT_LIMIT DECIMAL [0] NULL pin_decimal_t ptr
+2         PIN_FLD_CREDIT_THRESHOLDS_FIXED    STR [0] ""
+2         PIN_FLD_CREDIT_FLOOR DECIMAL [0] NULL pin_decimal_t ptr
+2         PIN_FLD_CREDIT_THRESHOLDS    INT [0] 0
+1     PIN_FLD_LIMIT         ARRAY [900007] allocated 20, used 4
+2         PIN_FLD_CREDIT_LIMIT DECIMAL [0] NULL pin_decimal_t ptr
+2         PIN_FLD_CREDIT_THRESHOLDS_FIXED    STR [0] ""
+2         PIN_FLD_CREDIT_FLOOR DECIMAL [0] NULL pin_decimal_t ptr
+2         PIN_FLD_CREDIT_THRESHOLDS    INT [0] 0
+1     PIN_FLD_LIMIT         ARRAY [1000001] allocated 20, used 4
+2         PIN_FLD_CREDIT_LIMIT DECIMAL [0] NULL pin_decimal_t ptr
+2         PIN_FLD_CREDIT_THRESHOLDS_FIXED    STR [0] ""
+2         PIN_FLD_CREDIT_FLOOR DECIMAL [0] NULL pin_decimal_t ptr
+2         PIN_FLD_CREDIT_THRESHOLDS    INT [0] 0
+1     PIN_FLD_LIMIT         ARRAY [1000011] allocated 20, used 4
+2         PIN_FLD_CREDIT_LIMIT DECIMAL [0] NULL pin_decimal_t ptr
+2         PIN_FLD_CREDIT_THRESHOLDS_FIXED    STR [0] ""
+2         PIN_FLD_CREDIT_FLOOR DECIMAL [0] NULL pin_decimal_t ptr
+2         PIN_FLD_CREDIT_THRESHOLDS    INT [0] 0
+1     PIN_FLD_BILLINFO_OBJ   POID [0] 0.0.0.1 /billinfo 1019773 0
+1     PIN_FLD_ACCOUNT_OBJ    POID [0] 0.0.0.1 /account 1018749 0
+1     PIN_FLD_SERVICE_OBJ    POID [0] 0.0.0.0  0 0
+0 PIN_FLD_CREATED_T    TSTAMP [0] (1388722147) Thu Jan  2 20:09:07 2014
+0 PIN_FLD_DESCR           STR [0] "Wireless Basic Package"
+0 PIN_FLD_ACCTINFO      ARRAY [0] allocated 20, used 7
+1     PIN_FLD_ACCOUNT_NO      STR [0] "SkyPS02"
+1     PIN_FLD_ACCESS_CODE2    STR [0] ""
+1     PIN_FLD_ACCESS_CODE1    STR [0] "access_code1"
+1     PIN_FLD_POID           POID [0] 0.0.0.1 /account -1 0
+1     PIN_FLD_DEAL_OBJ       POID [0] 0.0.0.0 / 0 0
+1     PIN_FLD_BAL_INFO      ARRAY [0]     NULL array ptr
+1     PIN_FLD_CURRENCY        INT [0] 840
+0 PIN_FLD_PROFILES      ARRAY [*] allocated 20, used 2
+1     PIN_FLD_PROFILE_OBJ    POID [0] 0.0.0.1 /profile/sky 1017853 0
+1     PIN_FLD_INHERITED_INFO SUBSTRUCT [0] allocated 20, used 1
+2         PIN_FLD_PROFILE_INFO SUBSTRUCT [0] allocated 20, used 2
+3             PIN_FLD_ORDER_TYPE      STR [0] "GLASS"
+3             SKY_FLD_CUSTOMER_ID     STR [0] "SkyPS02"
+0 PIN_FLD_START_T      TSTAMP [0] (1666351682) Fri Oct 21 04:28:02 2022
+0 PIN_FLD_END_T        TSTAMP [0] (1666351682) Fri Oct 21 04:28:02 2022
+0 PIN_FLD_GROUP_INFO   SUBSTRUCT [0] allocated 0, used 0
+0 PIN_FLD_ACCOUNT_OBJ    POID [0] 0.0.0.1 /account 1018749 0
+0 PIN_FLD_BILLINFO      ARRAY [0] allocated 20, used 8
+1     PIN_FLD_POID           POID [0] 0.0.0.1 /billinfo 1019773 0
+1     PIN_FLD_CURRENCY        INT [0] 840
+1     PIN_FLD_CURRENCY_SECONDARY    INT [0] 0
+1     PIN_FLD_EFFECTIVE_T  TSTAMP [0] (1666351682) Fri Oct 21 04:28:02 2022
+1     PIN_FLD_PAY_TYPE       ENUM [0] 0
+1     PIN_FLD_BILLINFO_ID     STR [0] "Default Bill"
+1     PIN_FLD_PAYINFO_OBJ    POID [0] NULL poid pointer
+1     PIN_FLD_BAL_GRP_OBJ    POID [0] 0.0.0.1 /balance_group 1018365 0
+0 PIN_FLD_HOST          ARRAY [1] allocated 20, used 2
+1     PIN_FLD_HOSTNAME        STR [0] "XXX.XXX.XXX.XXX"
+1     PIN_FLD_TYPE           ENUM [0] 1
+0 PIN_FLD_HOST          ARRAY [2] allocated 20, used 2
+1     PIN_FLD_HOSTNAME        STR [0] "XXX.XXX.XXX.XXX"
+1     PIN_FLD_TYPE           ENUM [0] 1
+0 PIN_FLD_HOST          ARRAY [3] allocated 20, used 3
+1     PIN_FLD_TYPE           ENUM [0] 2
+1     PIN_FLD_HOSTNAME        STR [0] "XXXXXXXXX.XXX"
+1     PIN_FLD_PORT            INT [0] 0
+0 PIN_FLD_HOST          ARRAY [4] allocated 20, used 2
+1     PIN_FLD_HOSTNAME        STR [0] "XXXX.XXX"
+1     PIN_FLD_TYPE           ENUM [0] 3
+0 PIN_FLD_HOST          ARRAY [5] allocated 20, used 2
+1     PIN_FLD_HOSTNAME        STR [0] "XXXX.XXX"
+1     PIN_FLD_TYPE           ENUM [0] 4
+0 PIN_FLD_HTTP_URL        STR [0] "XXXXXXXXXXXXXXX"
+0 PIN_FLD_SUPPORT_PHONE    STR [0] "XXXXXXXXXXXXXXX"
